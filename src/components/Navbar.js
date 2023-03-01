a37aab74d570867feed32915f9ec5afa87ca109a
firman-lab <fhidayat400@gmail.com>
2023-03-01 17:04:46 +0700

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,10 +14,10 @@ export default function Navbar() {
         />
         <h3 className="text-white text-xl font-semibold">Hoo<span className="text-[#00F6FF]">Bank</span></h3>
       </div>
-      <ul className="sm:flex hidden justify-end items-center flex-1">
+      <ul className="sm:flex hidden justify-end items-center flex-1 gap-9">
         {navLinks.map((nav, index) => (
 
-          <li key={nav.id}>{nav.title}</li>
+          <li className="text-white text-md font-semibold" key={nav.id}>{nav.title}</li>
         ))
         }
       </ul>
