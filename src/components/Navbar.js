16ff955c54d42128d718dad5e7e6d11ffc9c5133
Firman Hidayat <fhidayat400@gmail.com>
2023-03-01 15:55:21 +0700

    updte

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import { navLinks } from "@/constant";
+import Image from "next/image";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center">
+      <div className="flex flex-row justify-between items-center gap-2">
+        <Image
+          width={35}
+          height={35}
+          src={"/icons/hoobank.svg"}
+          alt={"HooBank Logo"}
+        />
+        <h3 className="text-white text-xl font-semibold">Hoo<span className="text-[#00F6FF]">Bank</span></h3>
+      </div>
+      <ul className="sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+
+          <li key={nav.id}>{nav.title}</li>
+        ))
+        }
+      </ul>
+    </nav>
+  );
+}
